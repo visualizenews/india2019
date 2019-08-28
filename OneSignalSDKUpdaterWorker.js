80a90e28e23193bd54cd502cb0a42edba2a007f0
Gitlab auto-deploy <213991+leeppolis@users.noreply.github.com>
2019-08-28 15:15:57 +0000

    Ready to deploy

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,3 @@
+/* eslint no-undef: 1 */
+/* eslint-env browser */
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
---
+++ precache-manifest.45c7c4ff0eb31403a7657b63c0254500.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7c0a27448e38490703",
+    "url": "/static/css/main.f3f614d0.chunk.css"
+  },
+  {
+    "revision": "3d7c0a27448e38490703",
+    "url": "/static/js/main.3d7c0a27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "f369d14eed56a0e37a22",
+    "url": "/static/js/2.f369d14e.chunk.js"
+  },
+  {
+    "revision": "722ba50771633a59b542728c42d602f4",
+    "url": "/static/media/vn.722ba507.png"
+  },
+  {
+    "revision": "1068a7f2f5dded22c41b207efc8642d7",
+    "url": "/static/media/modi.1068a7f2.png"
+  },
+  {
+    "revision": "7c3aeb21448f92f85c1eb3e357f0dd17",
+    "url": "/static/media/gandhi.7c3aeb21.png"
+  },
+  {
+    "revision": "7ac08f69a5578bae6360528739c62009",
+    "url": "/static/media/modi_mask.7ac08f69.png"
+  },
+  {
+    "revision": "948bfad0051c14ba4298a2e1bf2a7cf8",
+    "url": "/static/media/gandhi_mask.948bfad0.png"
+  },
+  {
+    "revision": "4e858512b8b852451b5ed376b31ff4bb",
+    "url": "/static/media/narendra-modi.4e858512.svg"
+  },
+  {
+    "revision": "205d10bcd526de4f7a3beee3e55feb97",
+    "url": "/static/media/rahul-gandhi.205d10bc.svg"
+  },
+  {
+    "revision": "98874f84951e70099489c74315592f96",
+    "url": "/static/media/mayawati.98874f84.svg"
+  },
+  {
+    "revision": "758a0aec9193c5419510ac0a24d6c020",
+    "url": "/static/media/akhilesh-yadav.758a0aec.svg"
+  },
+  {
+    "revision": "621487811be4031c5307d385e6cf9d76",
+    "url": "/static/media/mamata-banerjee.62148781.svg"
+  },
+  {
+    "revision": "4adfc2a7716c0a82a0933b510a872230",
+    "url": "/index.html"
+  }
+];
